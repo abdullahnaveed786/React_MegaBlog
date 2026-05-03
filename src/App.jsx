2e6a5c49d257920d1c7a78031810a26e5cb5034b
Abdullah Naveed <abdullahnaveeed01@gmail.com>
2026-05-03 16:05:14 +0500

    Vite Project Creation, Tailwind , @reduxjs/toolkit,  react-redux , react-router-dom,  appwrite , @tinymce/tinymce-react, html-react-parser, react-hook-form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold bg-amber-400 w-full">Welcome to the MegaBlog</h1>
+    </>
+  )
+}
+
+export default App
